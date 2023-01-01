4ec973c25ea2c8bdf46d87d00dd9d32ae9386b3d
Elnur <egarav88@mail.ru>
2023-01-01 13:09:15 +0400

    5.1.2.Итератор четных чисел [#150 #376476]

---
+++ src/main/java/ru/job4j/iterator/FlatMap.java
@@ -0,0 +1,34 @@
+package ru.job4j.iterator;
+
+import java.util.Collections;
+import java.util.Iterator;
+import java.util.List;
+
+public class FlatMap<T> implements Iterator<T> {
+    private final Iterator<Iterator<T>> data;
+    private Iterator<T> cursor = Collections.emptyIterator();
+
+    public FlatMap(Iterator<Iterator<T>> data) {
+        this.data = data;
+    }
+
+    @Override
+    public boolean hasNext() {
+        return false;
+    }
+
+    @Override
+    public T next() {
+        return null;
+    }
+
+    public static void main(String[] args) {
+        Iterator<Iterator<Integer>> data = List.of(
+                List.of(1, 2, 3).iterator(),
+                List.of(4, 5, 6).iterator(),
+                List.of(7, 8, 9).iterator()
+        ).iterator();
+        FlatMap flat = new FlatMap(data);
+        
+    }
+}
