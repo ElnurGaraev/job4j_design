9429dedcd7bac27c4547294d6d3f5239e1b7a39b
Elnur <egarav88@mail.ru>
2023-01-07 19:35:30 +0400

    5. Очередь на двух стеках [#160]

---
+++ src/main/java/ru/job4j/collection/SimpleQueue.java
@@ -0,0 +1,30 @@
+package ru.job4j.collection;
+
+import java.util.NoSuchElementException;
+
+public class SimpleQueue<T> {
+    private final SimpleStack<T> in = new SimpleStack<>();
+    private final SimpleStack<T> out = new SimpleStack<>();
+    int inSize = 0;
+    int outSize = 0;
+
+    public T poll() {
+        if (inSize == 0 && outSize == 0) {
+            throw new NoSuchElementException();
+        }
+        if (outSize == 0) {
+            while (inSize != 0) {
+                out.push(in.pop());
+                outSize++;
+                inSize--;
+            }
+        }
+        outSize--;
+        return out.pop();
+    }
+
+    public void push(T value) {
+        in.push(value);
+        inSize++;
+    }
+}
---
+++ src/test/java/ru/job4j/collection/SimpleQueueTest.java
@@ -0,0 +1,60 @@
+package ru.job4j.collection;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.NoSuchElementException;
+
+import static org.assertj.core.api.Assertions.*;
+
+class SimpleQueueTest {
+    private SimpleQueue<Integer> queue;
+
+    @BeforeEach
+    public void setUp() {
+        queue = new SimpleQueue<>();
+        queue.push(1);
+    }
+
+    @Test
+    void whenPushPoll() {
+        assertThat(queue.poll()).isEqualTo(1);
+    }
+
+    @Test
+    void when2PushPoll() {
+        queue.push(2);
+        assertThat(queue.poll()).isEqualTo(1);
+    }
+
+    @Test
+    void when2PushPollPushPoll() {
+        queue.poll();
+        queue.push(2);
+        assertThat(queue.poll()).isEqualTo(2);
+    }
+
+    @Test
+    void whenEmptyPoll() {
+        SimpleQueue<Integer> queue = new SimpleQueue<>();
+        assertThatThrownBy(() -> queue.poll())
+                .isInstanceOf(NoSuchElementException.class);
+    }
+
+    @Test
+    void when2PushPollPushPollEmpty() {
+        queue.poll();
+        queue.push(2);
+        queue.poll();
+        assertThatThrownBy(() -> queue.poll())
+                .isInstanceOf(NoSuchElementException.class);
+    }
+
+    @Test
+    void whenPushPushPollAndPush() {
+        queue.push(2);
+        queue.poll();
+        queue.push(3);
+        assertThat(queue.poll()).isEqualTo(2);
+    }
+}
