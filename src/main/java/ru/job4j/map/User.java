b023e8dece2450935d69344ff0feebad82111767
Elnur <egarav88@mail.ru>
2023-01-11 15:25:58 +0400

    3. Создать модель User [#999]

---
+++ src/main/java/ru/job4j/map/User.java
@@ -0,0 +1,15 @@
+package ru.job4j.map;
+
+import java.util.Calendar;
+
+public class User {
+    private String name;
+    private int children;
+    private Calendar birthday;
+
+    public User(String name, int children, Calendar birthday) {
+        this.name = name;
+        this.children = children;
+        this.birthday = birthday;
+    }
+}
