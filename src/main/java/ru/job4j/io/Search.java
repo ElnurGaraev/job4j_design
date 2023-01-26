0d9e5f62ba9213d2031fa2306b6e4addd10266c7
Elnur <egarav88@mail.ru>
2023-01-26 22:18:00 +0400

    13. Именованные аргументы [#295518]

--- src/main/java/ru/job4j/io/Search.java
+++ src/main/java/ru/job4j/io/Search.java
@@ -9,8 +9,16 @@ import java.util.function.Predicate;
 
 public class Search {
     public static void main(String[] args) throws IOException {
-        Path start = Paths.get(".");
-        List<Path> list = search(start, p -> p.toFile().getName().endsWith(".js"));
+        Search search = new Search();
+        search.validate(args);
+        Path start = Paths.get(args[0]);
+        List<Path> list = search(start, p -> p.toFile().getName().endsWith(args[1]));
+    }
+
+    public  void validate(String[] args) {
+        if (args.length == 0) {
+            throw new IllegalArgumentException("Root folder is null. Usage  ROOT_FOLDER.");
+        }
     }
 
     public static List<Path> search(Path root, Predicate<Path> condition) throws IOException {
