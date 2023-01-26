a4674e664c1a105b121b27654d0ab806ff47899f
Elnur <egarav88@mail.ru>
2023-01-26 20:25:01 +0400

    11. Поиск дубликатов [#315066]

--- src/main/java/ru/job4j/io/DuplicatesFinder.java
+++ src/main/java/ru/job4j/io/DuplicatesFinder.java
@@ -6,6 +6,8 @@ import java.nio.file.Path;
 
 public class DuplicatesFinder {
     public static void main(String[] args) throws IOException {
-        Files.walkFileTree(Path.of("./"), new DuplicatesVisitor());
+        DuplicatesVisitor dup = new DuplicatesVisitor();
+        Files.walkFileTree(Path.of("./"), dup);
+        dup.print(dup);
     }
 }
--- src/main/java/ru/job4j/io/DuplicatesVisitor.java
+++ src/main/java/ru/job4j/io/DuplicatesVisitor.java
@@ -5,20 +5,35 @@ import java.nio.file.FileVisitResult;
 import java.nio.file.Path;
 import java.nio.file.SimpleFileVisitor;
 import java.nio.file.attribute.BasicFileAttributes;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class DuplicatesVisitor extends SimpleFileVisitor<Path> {
-    private Map<FileProperty, Path> map = new HashMap<>();
+    private Map<FileProperty, List<Path>> map = new HashMap<>();
     @Override
     public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
         FileProperty fileProperty = new FileProperty(file.toFile().length(), file.toFile().getName());
-        if (!map.containsKey(fileProperty)) {
-            map.put(fileProperty, file);
-        } else {
-            System.out.println(String.format("File name: %s, file size:  %s", file.toFile().getName(), file.toFile().length()));
-            System.out.println(file.toAbsolutePath());
-        }
+        map.putIfAbsent(fileProperty, new ArrayList<>());
+        map.get(fileProperty).add(file);
         return super.visitFile(file, attrs);
+    }
+
+    public Map<FileProperty, List<Path>> getMap() {
+        return map;
+    }
+
+    public void print(DuplicatesVisitor duplicate) {
+        Map<FileProperty, List<Path>> mapDup = duplicate.getMap();
+        Map<FileProperty, List<Path>> rsl = mapDup.entrySet().stream()
+                .filter(e -> e.getValue().size() > 1)
+                .collect(Collectors.toMap(e -> e.getKey(), e -> e.getValue()));
+        for (Map.Entry<FileProperty, List<Path>> result : rsl.entrySet()) {
+            System.out.println(String.format("File name: %s, file size: %s",
+                    result.getKey().getName(), result.getKey().getSize()));
+            result.getValue().forEach(System.out::println);
+        }
     }
 }
