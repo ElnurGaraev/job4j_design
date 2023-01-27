f3752579f4a721c289e291eba87b9c40f8e01302
Elnur <egarav88@mail.ru>
2023-01-27 22:13:45 +0400

    14. Архивировать проект [#861]

--- src/main/java/ru/job4j/io/Zip.java
+++ src/main/java/ru/job4j/io/Zip.java
@@ -9,12 +9,12 @@ import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
 
 public class Zip {
-    public void packFiles(List<File> sources, File target) {
+    public void packFiles(List<Path> sources, File target) {
         try (ZipOutputStream zip = new ZipOutputStream(new BufferedOutputStream(
                 new FileOutputStream(target)))) {
-            for (File file : sources) {
-                zip.putNextEntry(new ZipEntry(file.getPath()));
-                try (BufferedInputStream out = new BufferedInputStream(new FileInputStream(file.getPath()))) {
+            for (Path path : sources) {
+                zip.putNextEntry(new ZipEntry(path.toFile().getPath()));
+                try (BufferedInputStream out = new BufferedInputStream(new FileInputStream(path.toFile().getPath()))) {
                     zip.write(out.readAllBytes());
                 }
             }
@@ -35,19 +35,31 @@ public class Zip {
         }
     }
 
+    public static void validate(String[] args) {
+        if (args.length == 0) {
+            throw new IllegalArgumentException("Argument is absent");
+        }
+        if (!(args[0].contains("=") && args[0].contains("-"))) {
+            throw new IllegalArgumentException(("Symbol in the first argument is not correct"));
+        }
+        if (!(args[1].contains("=") && args[1].contains("-"))) {
+            throw new IllegalArgumentException(("Symbol in the second argument is not correct"));
+        }
+        if (!(args[2].contains("=") && args[2].contains("-"))) {
+            throw new IllegalArgumentException(("Symbol in the third argument is not correct"));
+        }
+    }
+
     public static void main(String[] args) throws IOException {
         Zip zip = new Zip();
+        validate(args);
         ArgsName arg = ArgsName.of(args);
         Path source = Paths.get(arg.get("d"));
-        List<Path> list = Search.search(source, p -> !p.toFile().getName().contains(arg.get("e")));
-        List<File> files = new ArrayList<>();
-        for (Path path : list) {
-            files.add(path.toFile());
-        }
+        List<Path> list = Search.search(source, p -> !p.toFile().getName().endsWith(arg.get("e")));
         zip.packSingleFile(
                 new File("./pom.xml"),
                 new File("./pom.zip")
         );
-        zip.packFiles(files, new File(arg.get("o")));
+        zip.packFiles(list, new File(arg.get("o")));
     }
 }
