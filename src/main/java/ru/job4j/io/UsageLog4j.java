881e29e561328471e86de289555df2a5c477011b
Elnur <egarav88@mail.ru>
2023-02-03 16:49:09 +0400

    3. Slf4j - вывод переменных. [#268852]

--- src/main/java/ru/job4j/io/UsageLog4j.java
+++ src/main/java/ru/job4j/io/UsageLog4j.java
@@ -10,13 +10,15 @@ public class UsageLog4j {
     public static void main(String[] args) {
         String model = "Mercedes";
         byte month = 01;
+        short owners = 2;
         int year = 2019;
         char carClass = 'E';
         long  kilometre = 200000L;
         boolean accident = false;
         float width = 1.86F;
         double length = 2.94;
-        LOG.debug("Car info model : {}, month : {}, year : {},  carClass : {}", model, month, year, carClass);
+        LOG.debug("Car info model : {}, month : {}, year : {}, carClass : {}, owners : {}",
+                model, month, year, carClass, owners);
         LOG.error("Car characteristic width : {}, length : {}, kilometre : {}, accident : {}",
                 width, length, kilometre, accident);
     }
