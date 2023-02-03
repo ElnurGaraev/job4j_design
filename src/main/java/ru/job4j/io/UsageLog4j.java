40409f7277f2369dbb504ddab74e93fcc683c077
Elnur <egarav88@mail.ru>
2023-02-03 13:24:37 +0400

    1. Log4j. Логирование системы. [#95335]

--- src/main/java/ru/job4j/io/UsageLog4j.java
+++ src/main/java/ru/job4j/io/UsageLog4j.java
@@ -1,11 +1,10 @@
 package ru.job4j.io;
 
 
-import java.util.logging.LogManager;
-import java.util.logging.Logger;
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
 
 public class UsageLog4j {
-
     private static final Logger LOG = LogManager.getLogger(UsageLog4j.class.getName());
 
     public static void main(String[] args) {
