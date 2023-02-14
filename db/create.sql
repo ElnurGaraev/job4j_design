b66c47761afecdae94b95c41d48a9f966c0d6447
Elnur <egarav88@mail.ru>
2023-02-14 20:59:22 +0400

    1. Схема прав пользователи и роли. [#1093]

--- db/create.sql
+++ db/create.sql
@@ -22,32 +22,32 @@ rules_id int references rules(id)
 
 create table category(
 id serial primary key,
-name text,
+name text
 );
 
 create table state(
 id serial primary key,
-name text,
+name text
 );
 
 create table item(
 id serial primary key,
 name text,
-users_id int reference users(id)
-category_id int reference category(id)
-state_id int reference state(id)
+users_id int references users(id),
+category_id int references category(id),
+state_id int references state(id)
 );
 
 create table comments(
 id serial primary key,
 description text,
-item_id int reference item(id)
+item_id int references item(id)
 );
 
 create table attachs(
 id serial primary key,
 name text,
-item_id int reference item(id)
+item_id int references item(id)
 );
 
 
