8da112f1ecf86cbaaaa00a2ccc392a570e35c0a1
Elnur <egarav88@mail.ru>
2023-03-14 18:27:49 +0400

    4. Шаблонизатор. [#855]

---
+++ src/test/java/ru/job4j/template/GeneratorTest.java
@@ -0,0 +1,54 @@
+package ru.job4j.template;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashMap;
+
+import static org.assertj.core.api.Assertions.*;
+
+@Disabled
+class GeneratorTest {
+    @Test
+    public void whenPatternIsNotCorrect() {
+        GeneratorGreeting generator = new GeneratorGreeting();
+        assertThatThrownBy(
+                () -> generator.produce(new String(), new HashMap<>()))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("The pattern is not correct.");
+    }
+
+    @Test
+    public void whenKeyInThePatternIsAbsent() {
+        GeneratorGreeting generator = new GeneratorGreeting();
+        assertThatThrownBy(
+                () -> generator.produce(new String(), new HashMap<>()))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("Ключ в шаблоне отсутствует.");
+    }
+
+    @Test
+    public void whenValueInThePatternIsAbsent() {
+        GeneratorGreeting generator = new GeneratorGreeting();
+        assertThatThrownBy(
+                () -> generator.produce(new String(), new HashMap<>()))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("Значение в шаблоне отсутствует.");
+    }
+
+    @Test
+    public void whenKeyInTheMapIsAbsent() {
+        GeneratorGreeting generator = new GeneratorGreeting();
+        assertThatThrownBy(
+                () -> generator.produce(new String(), new HashMap<>()))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage("Ключ в карте отсутствует.");
+    }
+
+    @Test
+    public void whenGetPattern() {
+        GeneratorGreeting generator = new GeneratorGreeting();
+        String rsl = generator.produce(new String(), new HashMap<>());
+        assertThat(rsl).isEqualTo(new String());
+    }
+}
